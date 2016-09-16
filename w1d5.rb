a71ff0ce737b1f824fe74e52e099a4135d6fc68b
Melissa Lau <mlau@Melissas-MacBook-Pro.local>
2016-09-15 20:25:00 -0700

    create Queue class

--- w1d5.rb
+++ w1d5.rb
@@ -38,3 +38,27 @@ class Stack
     @stack_array
   end
 end
+
+# Exercise 2 - Queue
+#
+# Now let's write a Queue class. We will need the following instance methods: enqueue(el), dequeue, and show.
+#
+# Test your code to ensure it follows the principle of FIFO.
+
+class Queue
+  def initialize
+    @queue_array = []
+  end
+
+  def enqueue(el)
+    @queue_array.push(el)
+  end
+
+  def dequeue
+    @queue_array.shift
+  end
+
+  def show
+    @queue_array
+  end
+end

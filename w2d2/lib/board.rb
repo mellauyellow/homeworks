ecadf9c895727eeac4b935e0d402a588c70effe9
Melissa Lau <mlau@Melissas-MacBook-Pro.local>
2016-09-27 21:56:25 -0700

    w3d3

--- w2d2/lib/board.rb
+++ w2d2/lib/board.rb
@@ -3,45 +3,39 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
-    @name1 = name1
-    @name2 = name2
     @cups = Array.new(14) { [] }
+    @name1, @name2 = name1, name2
     place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
-    @cups[0..5].each do |cup|
-      4.times { cup << :stone }
-    end
-
-    @cups[7..12].each do |cup|
-      4.times { cup << :stone }
+    @cups.each_with_index do |cup, idx|
+      next if idx == 6 || idx == 13
+      cup.push(:stone, :stone, :stone, :stone)
     end
   end
 
   def valid_move?(start_pos)
-    raise "Invalid starting cup" unless start_pos.between?(1, 12)
+    raise "Invalid starting cup" unless start_pos.between?(1, @cups.length)
   end
 
   def make_move(start_pos, current_player_name)
-    selected_cup = @cups[start_pos]
+    temp = @cups[start_pos]
     @cups[start_pos] = []
-    player_store = current_player_name == @name1 ? 6 : 13
-    opp_store = current_player_name == @name1 ? 13 : 6
 
-    cup_idx = start_pos
-    until selected_cup.empty?
-      cup_idx += 1
-      cup_idx = 0 if cup_idx == 14
-      cups[cup_idx] << selected_cup.shift unless cup_idx == opp_store
+    start = start_pos
+    until temp.empty?
+      start += 1
+      start = 0 if start == 14
+      unless (start == 6 && current_player_name == @name2) || (start == 13 && current_player_name == @name1)
+        @cups[start] << temp.shift
+      end
     end
 
-    ending_pos = cup_idx
-
-    return :prompt if ending_pos == player_store
-    return :switch if @cups[ending_pos].length == 1
-    return ending_pos
+    return :prompt if (start == 6 && current_player_name == @name1) || (start == 13 && current_player_name == @name2)
+    return :switch if @cups[start].length == 1
+    return start if @cups[start].length > 1
   end
 
   def next_turn(ending_cup_idx)
@@ -57,12 +51,12 @@ class Board
   end
 
   def cups_empty?
-    @cups[0..5].all? { |cup| cup.empty? } || @cups[7..12].all? { |cup| cup.empty? }
+    return true if @cups[0..5].all? { |cup| cup.empty? } || @cups[7..12].all? { |cup| cup.empty? }
+    false
   end
 
   def winner
     return :draw if @cups[6].length == @cups[13].length
-
     @cups[6].length > @cups[13].length ? @name1 : @name2
   end
 end
